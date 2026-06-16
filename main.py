318edbc12b144d9990d85d238873c0a49bc1e078
Gabriel Max <dev.gabrielmax@gmail.com>
2026-06-16 12:59:06 -0300

    Setup inicial do app. (Nomes, Cabeçalho, dados ficticios, registros de memoria)

---
+++ main.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+import json
+import os
+
+# Constantes Génericas para o Controle
+ARQUIVO_MEMORIA = "dados_sistema.json"
+ARQUIVO_LOG = "registro_ponto.txt"
+
+class AppPonto:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Controle de Entrada e Sáida")
+        self.root.geometry("1050x700")
+        
+        # Verifica e cria os dados fictícios se for a primeira vez rodando.
+        self.preparar_ambiente_ficticio()
+        
+        # Cabeçalho Principal
+        titulo = tk.label(root, text="Controle de Entrada e Saída", font=("Arial", 16, "bold"))
+        titulo.pack(pady=10)
+        
+        # Assinatura
+        assinatura = tk.Label(root, text="© Gabriel Max - Desenvolvido para Portfólio", font=("Arial", 9, "italic"), fg="gray")
+        assinatura.pack(side="bottom", pady=5)
+        
+        # ----------------- LAYOUT BASE (Wireframe) -----------------
+        self.container_esquerda = tk.Frame(root, bd=2, relief="groove")
+        self.container_esquerda.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+        
+        tk.Label(self.container_esquerda, text="Lista de Colaboradores (Em Construção)", font=("Arial", 12), fg="gray").pack(pady=250)
+
+        self.frame_lateral = tk.Frame(root, bd=2, relief="groove", padx=10, pady=10)
+        self.frame_lateral.pack(side="right", fill="y", padx=15, pady=10)
+        
+        tk.Label(self.frame_lateral, text="Painel de Ferramentas", font=("Arial", 12), fg="gray").pack(pady=250)
+        
+        
+    def preparar_ambiente_ficticio(self):
+        # Cria um banco de dados de teste automaticamente se for a primeira execução
+        if not os.path.exists(ARQUIVO_MEMORIA):
+            dados_padrao = {
+                "Dev Pleno": {is_in": False, "obs": "", "status": Ativo"},
+                "Dev Backend": {is_in": False, "obs": "", "status": Ativo"},
+                "Dev Senior": {is_in": False, "obs": "", "status": Ativo"},
+                "Estagiário": {is_in": False, "obs": "", "status": Ativo"},
+                "Dev Full Stack": {is_in": False, "obs": "", "status": Ativo"},
+                "Gerente de Projetos": {is_in": False, "obs": "", "status": Ativo"},
+            }
+            try:
+                with open (ARQUIVO_MEMORIA, "w", encoding="utf-8") as f:
+                    json.dump(dados_padrao, f, ensure_ascii=False, indent=4)
+                print("Banco de dados fictício criado com sucesso!")
+            except Exception as e:
+                print(f"Erro ao criar banco de dados inicial: {e}")
+                
+if __name__m == "__main__":
+    root = tk.TK()
+    app = AppPonto(root)
+    root.mainloop()                            
+        
+        
+        
